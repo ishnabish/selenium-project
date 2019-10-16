ca34df6c6ed280a6786414ee3e4fd2b68f06d81e
ishnabish <ishnabish@gmail.com>
2019-10-16 17:28:06 +0300

    +

--- tests/pytest/test_xunit_style_module.py
+++ tests/pytest/test_xunit_style_module.py
@@ -1,3 +1,6 @@
+import time
+
+import pytest
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 driver: WebDriver = None
@@ -18,10 +21,9 @@ def teardown_module():
 
 def test_module_setup_driver():
     print('test invocation started')
-    driver.get('http://2ch.hk')
-    search_input = driver.find_element_by_xpath('//li[@href="/b/"]')
+    driver.get('https://localbitcoins.net/ru/')
+    search_input = driver.find_element_by_xpath('//li[@id="top-login-link"]')
     search_input.click()
 
-    import time
-    time.sleep(3)
+    time.sleep(5)
     assert True
