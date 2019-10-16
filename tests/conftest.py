64028924060323b64ffe80979823904fa5aa6889
ishnabish <ishnabish@gmail.com>
2019-10-16 14:33:25 +0300

    +

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+@pytest.fixture
+def driver(request):
+    print('start fixture')
+    chrome_driver = WebDriver(executable_path='D://selenium//chromedriver.exe')
+    chrome_driver.implicitly_wait(3)
+    yield chrome_driver
+    print('stop fixture')
+    chrome_driver.close()
+
+        ##
